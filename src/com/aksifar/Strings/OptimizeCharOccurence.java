cf246db042f25f381b694d18a111409617c6a368
ankit ranjan <ankit.ranjan@accolitelabs.com>
2019-01-12 16:22:09 +0530

    fixed char occurence

--- src/com/aksifar/Strings/OptimizeCharOccurence.java
+++ src/com/aksifar/Strings/OptimizeCharOccurence.java
@@ -11,77 +11,130 @@ package com.aksifar.Strings;
 public class OptimizeCharOccurence {
 
 	public static void main(String[] args) {
-		int A= 5;
-		int B =2;
+		int A= 10;
+		int B =4;
 		System.out.println(solution(A,B));
 	}
 	
 	public static String solution(int A, int B)
 	{
-		String result="";
 		if(A == B )
 		{
-			for(int i=1; i<=A; i++)
+			String result="";
+			for(int i=1; i <= A; i++)
 				result = result + "ab";
 			return result;
 		}
-		else if (A > B)
+		else if (A > B) return ab(A,B);
+		else return ba(A,B); 
+	}
+	
+	public static String ab (int A, int B){
+		String result="";
+		String temp="";
+		for(; A+B>0;)
 		{
-			String temp="";
-			for(; A+B>=0;)
+			if(A >= 2)
 			{
-				if(A >= 2)
+				temp = temp + "aa";
+				if(temp.contains("aaa"))
+					temp = result;
+				else
 				{
-					temp = temp + "aa";
-					if(temp.contains("aaa"))
-						temp = result;
-					else
-					{
-						A = A-2;
-						result = temp;
-					}
+					A = A-2;
+					result = temp;
 				}
-				else if( A > 0)
+			}
+			else if( A > 0)
+			{
+				temp = temp + "a";
+				if(temp.contains("aaa"))
+					temp = result;
+				else
 				{
-					temp = temp + "a";
-					if(temp.contains("aaa"))
-						temp = result;
-					else
-					{
-						A = A-1;
-						result = temp;
-					}
-					
+					A = A-1;
+					result = temp;
 				}
-				if(B >= 2)
+			}
+			if(B >= 2 && !(A+2>=2*B))
+			{
+				temp = temp + "bb";
+				if(temp.contains("bbb"))
+					temp = result;
+				else
 				{
-					temp = temp + "bb";
-					if(temp.contains("bbb"))
-						temp = result;
-					else
-					{
-						B = B-2;
-						result = temp;
-					}
+					B = B-2;
+					result = temp;
 				}
-				else if (B > 0)
+			}
+			else if (B > 0)
+			{
+				temp = temp + "b";
+				if(temp.contains("bbb"))
+					temp = result;
+				else
 				{
-					temp = temp + "b";
-					if(temp.contains("bbb"))
-						temp = result;
-					else
-					{
-						B= B-1;
-						result = temp;
-					}
+					B= B-1;
+					result = temp;
 				}
-				
-				result = temp;
+			}
+			result = temp;
+		}
+	return result;
+	}
+	
+	public static String ba(int A, int B){
+		String result="";
+		String temp="";
+		for(; B+A>0;)
+		{
+			if(B >= 2)
+			{
+				temp = temp + "bb";
+				if(temp.contains("bbb"))
+					temp = result;
+				else
+				{
+					B = B-2;
+					result = temp;
+				}
+			}
+			else if( B > 0)
+			{
+				temp = temp + "b";
+				if(temp.contains("bbb"))
+					temp = result;
+				else
+				{
+					B = B-1;
+					result = temp;
+				}
 				
 			}
+			if(A >= 2 && !(B+2>=2*A))
+			{
+				temp = temp + "aa";
+				if(temp.contains("aaa"))
+					temp = result;
+				else
+				{
+					A = A-2;
+					result = temp;
+				}
+			}
+			else if (A > 0)
+			{
+				temp = temp + "a";
+				if(temp.contains("aaa"))
+					temp = result;
+				else
+				{
+					A= A-1;
+					result = temp;
+				}
+			}
+			result = temp;
 		}
-		
-		return result;
+	return result;
 	}
-
 }
